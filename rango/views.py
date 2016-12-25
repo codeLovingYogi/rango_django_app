937ed5c12bd8e789f46a05e1ea2be142586c0c8e
Felice H <yogilovingcode@gmail.com>
2016-12-25 17:48:47 -0500

    Update app views.py to rid 'The context_instance argument of render_to_string is deprecated.' message

--- rango/views.py
+++ rango/views.py
@@ -1,18 +1,13 @@
 from django.http import HttpResponse
-from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def index(request):
-	# Request the context of the request
-	# The context contains info such as the client's machine details, etc
-	context = RequestContext(request)
-
 	# Construct a dictionary to pass to the template engine as its context.
 	context_dict = {'boldmessage': "I am bold front from the context"}
 
 	# Request a rendered request to send to the client
 	# Making use of shortcut function to make our lives easier
-	return render_to_response('rango/index.html', context_dict, context)
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	return HttpResponse("<h1>Rango says: Here is the About page.</h1> <a href='/rango/'>Index</a>")
