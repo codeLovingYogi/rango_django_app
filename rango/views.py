afa3f2c969cc07a4aa3a893886a378cceab1d81d
Felice H <yogilovingcode@gmail.com>
2016-12-25 15:42:43 -0500

    Add About page - view and url

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello world!")
+	return HttpResponse("<h1>Rango says hello world!</h1> <a href='/rango/about'>About</a>")
+
+def about(request):
+	return HttpResponse("<h1>Rango says: Here is the About page.</h1> <a href='/rango/'>Index</a>")
