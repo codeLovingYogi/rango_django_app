85c28ac40775da75f9bb8220e76c5288f6876c7e
Felice H <yogilovingcode@gmail.com>
2016-12-26 12:07:29 -0500

    Update index page to access data from models within views and present data via templates

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	context_dict = {'boldmessage': "I am bold front from the context"}
+	# Query database for a list of all categories currently stored
+	# Order categories by number of likes in descending order
+	# Retrieve top 5 only, or all if less than 5
+	# Place list in context_dict dictionary which will be passed to template engine
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 
 	# Request a rendered request to send to the client
 	# Making use of shortcut function to make our lives easier
