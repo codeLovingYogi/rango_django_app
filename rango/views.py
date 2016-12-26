1d84af3922543f75215ee9bedd70ce75bbce374d
Felice H <yogilovingcode@gmail.com>
2016-12-25 21:25:54 -0500

    Convert About page to use template and static media

--- rango/views.py
+++ rango/views.py
@@ -10,4 +10,4 @@ def index(request):
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	return HttpResponse("<h1>Rango says: Here is the About page.</h1> <a href='/rango/'>Index</a>")
+	return render(request, 'rango/about.html')
